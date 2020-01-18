4b6c408f6004198ebb0fe1ca9733b05027b73395
William Soukkachang <Wsoukkachang@yahoo.com>
2020-01-17 21:59:30 -0600

    Installed Semantic UI React and Styled Components. Styled Login component. Working on Registration Component.

--- src/Components/Auth/Login.js
+++ src/Components/Auth/Login.js
@@ -1,73 +1,87 @@
 import React, { Component } from "react";
 
 import { Link } from "react-router-dom";
-import axios from "axios"
+import axios from "axios";
+import {
+  PageDiv,
+  LoginContainer,
+  LoginInputsContainer,
+  Input,
+  Label,
+  H1,
+  Button
+} from "./login-style.js";
+import { Image, Form} from "semantic-ui-react";
 
 class Login extends Component {
-    state = {
-        
-          username: "",
-          password: "",
-          submitted: false
-      };
+  state = {
+    username: "",
+    password: "",
+    submitted: false
+  };
 
-      handleChange = e => {
-        this.setState({
-            [e.target.name]: e.target.value
-          
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  handleLogin = e => {
+    e.preventDefault();
+    const credentials = {
+      username: this.state.username,
+      password: this.state.password
+    };
+    this.setState({ submitted: true });
+    if (this.state.username && this.state.password) {
+      axios
+        .post("https://lambda-mud-test.herokuapp.com/api/login/", credentials)
+        .then(res => {
+          localStorage.setItem("key", res.data.key);
+          this.setState({
+            username: "",
+            password: ""
+          });
+          this.props.history.push(`/re`);
         });
-      };
-
-      handleLogin = e => {
-        e.preventDefault();
-        const credentials = {
-            username: this.state.username,
-            password: this.state.password
-        }
-        this.setState({ submitted: true });
-        if(this.state.username && this.state.password ){
-        axios
-          .post("https://lambda-mud-test.herokuapp.com/api/login/", credentials)
-          .then(res =>{
-              localStorage.setItem("key", res.data.key);
-              this.setState({
-                username: '',
-                password: '',
-              });
-              this.props.history.push(`/re`);
-          })
-      }
     }
+  };
 
   render() {
     return (
-      <div>
-        <div className="main">
-          <p className="sign">
-            {" "}
-          </p>
-          <form onSubmit={this.handleLogin}>
+      <PageDiv className="main">
+        <p className="sign"> </p>
+        <H1>Sign In</H1>
+        <Form onSubmit={this.handleLogin}>
+          <LoginContainer>
+            <LoginInputsContainer>
+              <Form.Field>
+              <div>
+                {this.props.error && (
+                  <div className="help-block"> Invalid Credentials </div>
+                )}
+
+                <Label>Username</Label>
+
+                <Input
+                  type="text"
+                  name="username"
+                  value={this.state.username}
+                  onChange={this.handleChange}
+                  className="input"
+                  placeholder="User Name"
+                />
+
+                {this.state.submitted && !this.state.username && (
+                  <div className="help-block"> Username is required</div>
+                )}
+              </div>
+            </Form.Field>
+
+            <Form.Field>
             <div>
-              {this.state.submitted && !this.state.username && (
-                <div className="help-block">Username is required</div>
-              )}
-              {this.props.error && (
-                <div className="help-block">Invalid Credentials </div>
-              )}
-              <input
-                type="text"
-                name="username"
-                value={this.state.username}
-                onChange={this.handleChange}
-                className="input"
-                placeholder="User Name"
-              />
-            </div>
-            <div>
-              {this.state.submitted && !this.state.password && (
-                <div className="help-block">Password is required</div>
-              )}
-              <input
+              <Label>Password</Label>
+              <Input
                 type="password"
                 name="password"
                 value={this.state.password}
@@ -75,19 +89,38 @@ class Login extends Component {
                 className="input"
                 placeholder="Password"
               />
+
+              {this.state.submitted && !this.state.password && (
+                <div className="help-block"> Password is required</div>
+              )}
             </div>
-            <button className="submit">Log in</button>
-          </form>
-          <div className="forgot">
-            <a className="helpLink" href="/">
-              Forgot Password?
-            </a>
-            <Link className="helpLink" to="/register">
-              Sign Up
-            </Link>
-          </div>
-        </div>
-      </div>
+            </Form.Field>
+            </LoginInputsContainer>
+
+            <Button class="ui fluid button" className="submit">Log in</Button>
+
+            <Image
+              // onClick={this.toggleVisibility}
+              src="https://www.imageupload.net/upload-image/2020/01/18/login-gif.gif"
+              as="a"
+              size="large"
+              // href="/login"
+              id="loginImage"
+            />
+
+            <div className="forgot">
+              <Link className="helpLink" href="/">
+                Forgot Password?
+              </Link>
+            </div>
+            <div>
+              <Link className="helpLink" to="/register">
+                Sign Up
+              </Link>
+            </div>
+          </LoginContainer>
+        </Form>
+      </PageDiv>
     );
   }
 }
---
+++ src/Components/Auth/login-style.js
@@ -0,0 +1,159 @@
+import styled, { keyframes } from "styled-components"; //for styling
+import { bounce } from "react-animations"; //for animation
+
+const bounceAnimation = keyframes`${bounce}`;
+
+// Bouncing Animation
+const BouncyDiv = styled.div`
+  animation: 1s ${bounceAnimation};
+`;
+
+// PageDiv Styling //
+export const PageDiv = styled.div`
+  //   background: #00FFAB;
+
+  //aqua green background----//
+//   background: radial-gradient(
+//         1.5em 6.28571em at 1.95em,
+//         rgba(255, 255, 255, 0) 50%,
+//         rgba(255, 255, 255, 0.25) 50%,
+//         rgba(255, 255, 255, 0.25) 55%,
+//         rgba(255, 255, 255, 0) 55%
+//       )
+//       0 0,
+//     radial-gradient(
+//         1.5em 6.28571em at -0.45em,
+//         rgba(255, 255, 255, 0) 50%,
+//         rgba(255, 255, 255, 0.25) 50%,
+//         rgba(255, 255, 255, 0.25) 55%,
+//         rgba(255, 255, 255, 0) 55%
+//       )
+//       1.5em 5.5em,
+//     radial-gradient(
+//         2.3em 4.57143em at 2.99em,
+//         rgba(255, 255, 255, 0) 50%,
+//         rgba(255, 255, 255, 0.3) 50%,
+//         rgba(255, 255, 255, 0.3) 55%,
+//         rgba(255, 255, 255, 0) 55%
+//       )
+//       0 0,
+//     radial-gradient(
+//         2.3em 4.57143em at -0.69em,
+//         rgba(255, 255, 255, 0) 50%,
+//         rgba(255, 255, 255, 0.3) 50%,
+//         rgba(255, 255, 255, 0.3) 55%,
+//         rgba(255, 255, 255, 0) 55%
+//       )
+//       2.3em 4em,
+//     radial-gradient(
+//         3.5em 6.28571em at 4.55em,
+//         rgba(255, 255, 255, 0) 50%,
+//         rgba(255, 255, 255, 0.25) 50%,
+//         rgba(255, 255, 255, 0.25) 55%,
+//         rgba(255, 255, 255, 0) 55%
+//       )
+//       0 0,
+//     radial-gradient(
+//         3.5em 6.28571em at -1.05em,
+//         rgba(255, 255, 255, 0) 50%,
+//         rgba(255, 255, 255, 0.25) 50%,
+//         rgba(255, 255, 255, 0.25) 55%,
+//         rgba(255, 255, 255, 0) 55%
+//       )
+//       3.5em 5.5em,
+//     radial-gradient(#15ffa5, #00ced1);
+//   background-color: mediumspringgreen;
+//   background-size: 1.5em 11em, 1.5em 11em, 2.3em 8em, 2.3em 8em, 3.5em 11em,
+//     3.5em 11em, 100% 100%;
+//   background-repeat: repeat;
+
+  //blue background
+  background: radial-gradient(ellipse at bottom, #5091DD 0%, #030617 100%);
+
+  padding-bottom: 100px;
+
+  //green bg border
+//   border: 20px double #02cac7;
+  
+  //blue bg border
+  border: 20px double navy;
+`;
+
+// Form styling //
+export const FormGroup = styled.form`
+  background: whitesmoke;
+  display: block;
+  margin: 1rem auto;
+`;
+
+// Login container styling //
+export const LoginContainer = styled.div`
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+  
+  align-content: space-around;
+  align-items: center;
+  margin: 0 auto;
+  padding: 2rem;
+//   border: 1px solid red;
+
+  @media (max-width: 700px) {
+    width: 100%;
+  }
+`;
+
+// Login Inputs container styling//
+export const LoginInputsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+  align-items: center;
+  // border: 1px solid red;
+  width: 100%;
+`;
+
+// Input label styling //
+export const Label = styled.label`
+  display: flex;
+  font-weight: bold;
+  text-align: left !important;
+  margin: 0 auto;
+//   color: #0521b0 !important;
+  color: white !important;
+  padding: 0.25em;
+  max-width: 250px;
+`;
+
+// Input Styling //
+export const Input = styled.input`
+  padding: 1rem;
+  color: white !important;
+  background: white !important;
+  border: none;
+  border-radius: 3px;
+  width: 100% !important;
+  height: 15px !important;
+  margin: 0.5rem, 0.5rem;
+  animation: 1s ${bounceAnimation};
+`;
+
+// Button Styling //
+export const Button = styled.button`
+    margin-top: 25px !important;
+    text-align: center;
+    padding: 1em 2em;
+    // color: white !important;
+    border-radius: 3px;
+    margin-left: 35px;
+    align-items: center;
+
+`;
+
+// h1 styling //
+export const H1 = styled.h1`
+  text-align: center;
+  color: #FFF29D;
+  margin: 0 auto;
+  padding-top: 50px;
+`;
